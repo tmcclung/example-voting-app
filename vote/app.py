6f55f86b403e9c77f51ecc7a60984db5286852fb
Tommy McClung <tmcclung@gmail.com>
2020-02-10 14:12:26 -0800

    cofactor 2

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Cofactor")
-option_b = os.getenv('OPTION_B', "Not Cofactor")
+option_b = os.getenv('OPTION_B', "Cofactor 2")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
