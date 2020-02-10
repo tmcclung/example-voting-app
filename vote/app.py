cf90aa784fc078906c2148d33d9968f9f284baba
Tommy McClung <tmcclung@gmail.com>
2020-02-10 09:33:44 -0800

    app.py for simon

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Sweeps")
-option_b = os.getenv('OPTION_B', "Observe Point")
+option_a = os.getenv('OPTION_A', "Simon")
+option_b = os.getenv('OPTION_B', "Braze")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
